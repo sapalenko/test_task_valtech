ccd75f7007e56c233cb5aab45293c50425d3e9ae
sapalenko <sapalenko1996@gmail.com>
2019-03-20 13:57:24 +0300

    html, css, script

---
+++ js/main.js
@@ -0,0 +1,20 @@
+<script type="text/javascript">
+ $(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+    (function($) {  
+	$(function() {  
+   
+ 	$('#up').click(function() {  
+    $('body,html').animate({scrollTop:0},500);  
+    return false;  
+  			})  
+		})  
+	})(jQuery)  
+});
+</script>
