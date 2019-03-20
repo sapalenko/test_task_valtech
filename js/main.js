d147b2f201f1c63ebd16552482a8186df8ab6ca3
sapalenko <sapalenko1996@gmail.com>
2019-03-21 01:55:11 +0300

    correct html, css

--- js/main.js
+++ js/main.js
@@ -4,17 +4,7 @@
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
+        $('body,html').animate({scrollTop: top}, 2000);
     });
-
-    (function($) {  
-	$(function() {  
-   
- 	$('#up').click(function() {  
-    $('body,html').animate({scrollTop:0},500);  
-    return false;  
-  			})  
-		})  
-	})(jQuery)  
 });
 </script>
